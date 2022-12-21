10cfec1924b920e318c9984c5a864ff3c2dd0273
Rafael Correa <scriptsengineer@gmail.com>
2022-12-21 08:14:16 -0300

    Update to godot 4 beta 9

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -5,12 +5,13 @@
 
 #include "register_types.h"
 
-#include <godot/gdnative_interface.h>
+#include <gdextension_interface.h>
 
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
 
+#include "example.h"
 #include "gdexample.h"
 #include "tests.h"
 
@@ -21,6 +22,11 @@ void initialize_example_module(ModuleInitializationLevel p_level) {
 		return;
 	}
 
+	ClassDB::register_class<ExampleRef>();
+	ClassDB::register_class<ExampleMin>();
+	ClassDB::register_class<Example>();
+	ClassDB::register_class<ExampleVirtual>(true);
+	ClassDB::register_abstract_class<ExampleAbstract>();
 	ClassDB::register_class<GDExample>();
 }
 
@@ -32,7 +38,7 @@ void uninitialize_example_module(ModuleInitializationLevel p_level) {
 
 extern "C" {
 // Initialization.
-GDNativeBool GDN_EXPORT example_library_init(const GDNativeInterface *p_interface, GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
+GDExtensionBool GDE_EXPORT example_library_init(const GDExtensionInterface *p_interface, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
 	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
 
 	init_obj.register_initializer(initialize_example_module);
